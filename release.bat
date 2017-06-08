d50b0db7264209c5f5c609be08a433d82abeb360
David Betteridge <david.betteridge>
2017-06-08 15:54:17 +0100

    Troubleshooting

--- release.bat
+++ release.bat
@@ -3,17 +3,17 @@
 if [%1]==[] goto usage
 
 @echo on
-docker run -it -v //C/proactis-documentation/documentation-source:/content -v //C/proactis-documentation/proactis-documentation.github.io:/proactis-documentation.github.io -p 8001:8000 proactis/mkdocs:1 build
+docker run -it -v //C/code/documentation-source:/content -v //C/code/proactis-documentation.github.io:/proactis-documentation.github.io -p 8001:8000 proactis/mkdocs:1 build
 
 @rem Push the generated site
-PUSHD  C:\proactis-documentation\proactis-documentation.github.io
+PUSHD  C:\code\proactis-documentation.github.io
 git add -A
 git commit -m "%1"
 git push
 POPD
 
 @rem Push the source code for the site
-PUSHD  C:\proactis-documentation\documentation-source
+PUSHD  C:\code\documentation-source
 git add -A
 git status
 git commit -m "%1"
